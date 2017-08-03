d9348b58b3e8f32a1c9735d7091c8424dd25f28c
Leo Lee <spleolee@gmail.com>
2017-08-04 00:31:06 +0800

    update sql

--- db/create_table.sql
+++ db/create_table.sql
@@ -7,6 +7,3 @@ CREATE TABLE web_visit
  file_timestamp VARCHAR(14));
  
 CREATE INDEX idx_web_visit_vist_count ON web_visit (file_timestamp, record_date, visit_count desc, web_site);
-
-CREATE TABLE web_visit_source_version
-(current_file_timestamp VARCHAR(14)); 
